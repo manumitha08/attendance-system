69cbaf9637dde1e9ed21667bcbc6c9488d32ef3f
manumitha08 <manumitha862000@gmail.com>
2022-12-12 16:56:24 +0530

    online_attendance_system

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+    $DB_HOST = "localhost";
+    $DB_USERNAME = "root";
+    $DB_PASSWORD = "root";
+    $DB_NAME = "attendance_system";
+
+    $conn = mysqli_connect(
+        $DB_HOST,
+        $DB_USERNAME,
+        $DB_PASSWORD,
+        $DB_NAME
+    );
+?>
